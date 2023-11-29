726cc3ccf32fa9296a1e94524b1007b871abe4f5
Indra <119665395+Kurosvki@users.noreply.github.com>
2023-11-29 12:56:06 +0100

    Aggiornato fcfs con commenti

--- FcFs.java
+++ FcFs.java
@@ -9,24 +9,37 @@ public class FcFs {
 	public String Fcfs(String x) {
 		return x;
 	}
-	//Assegno un nome ai processi	
+	/**
+	 * Assegna un nome ai processi
+	 * @param x scorre la lunghezza del processo e assegna un nome
+	 */
 	public void setP() {
 		for(int x=0;x<this.processi.length;x++) {
 			this.processi[x]+=x;
 		}
 	}
-	//Prendo il tempo di arrivo
+	/**
+	 * Prendo il tempo di arrivo
+	 * @param x scorre tempo di arrivo
+	 * @return ritorna il tempo di arrivo
+	 */
 	public int[] getTarrivo() {
 		for(int x=0;x<tArrivo.length;x++) {
 			tArrivo[x]=x;
 		}
 		return tArrivo;
 	}
-	//Tempo che ci mette per essere eseguito
+	/**
+	 * Tempo che ci mette per essere eseguito
+	 */
 	public void setTesecuzione(int[] te) {
 		this.tEsecuzione = te;
 	}
-	//Calcolo del tempo che server per essere eseguito
+	/**
+	 * Calcolo del tempo che server per essere eseguito
+	 * @param x scorre il tempo di servizio
+	 * @return ritorna il tempo di servizio
+	 */
 	public int[] getTs() {
 		for (int x=0;x<tServizio.length;x++) {
 			tServizio[x]=tEsecuzione[x]+tArrivo[x-1];
